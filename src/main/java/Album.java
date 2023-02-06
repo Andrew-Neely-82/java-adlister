2331c44e70aee198d13e22f1eb0c753314bc7feb
Andrew-Neely-82 <114946953+Andrew-Neely-82@users.noreply.github.com>
2023-02-06 13:40:09 -0600

    added

---
+++ src/main/java/Album.java
@@ -0,0 +1,70 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+  private long id;
+  private String artist;
+  private String name;
+  private int year;
+  private float sales;
+  private String genre;
+
+  public Album() {
+  }
+
+  public Album(long id, String artist, String name, int releaseDate, int sales, String genre) {
+    this.id = id;
+    this.artist = artist;
+    this.name = name;
+    this.year = releaseDate;
+    this.sales = sales;
+    this.genre = genre;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public void setReleaseDate(int releaseDate) {
+    this.year = releaseDate;
+  }
+
+  public float getSales() {
+    return sales;
+  }
+
+  public void setSales(float sales) {
+    this.sales = sales;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+}
